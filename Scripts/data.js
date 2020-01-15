36b5184f3f749be367b404ea6e5bc697ad95874e
Merit17 <59911717+Merit17@users.noreply.github.com>
2020-01-15 17:00:21 +0100

    2e kaart
    2e kaart

---
+++ Scripts/data.js
@@ -0,0 +1,10 @@
+var indusdata = {
+"type": "FeatureCollection",
+"name": "Indus",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID": 1.0, "naam": "vervang mij "}, "geometry": { "type": "Point", "coordinates": [ 68, 24 ] } },
+{ "type": "Feature", "properties": { "ID": 2.0, "naam": "mijnnaam" }, "geometry": { "type": "Point", "coordinates": [ -58.400969556016499, -34.629375657572822 ] } },
+{ "type": "Feature", "properties": { "ID": 3.0, "naam": "tekst" }, "geometry": { "type": "Point", "coordinates": [ -58.3704125010713, -34.603013905923149 ] } }
+]
+}
