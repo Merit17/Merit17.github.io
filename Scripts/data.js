ad7561e69de07c30300fd0ac8195317dd3f0ca04
Merit17 <59911717+Merit17@users.noreply.github.com>
2020-01-19 16:59:23 +0100

    Update data js

--- Scripts/data.js
+++ Scripts/data.js
@@ -3,8 +3,9 @@ var indusdata = {
 "name": "Indus",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "ID": 1.0, "naam": "vervang mij "}, "geometry": { "type": "Point", "coordinates": [ 68, 24 ] } },
-{ "type": "Feature", "properties": { "ID": 2.0, "naam": "mijnnaam" }, "geometry": { "type": "Point", "coordinates": [ -58.400969556016499, -34.629375657572822 ] } },
-{ "type": "Feature", "properties": { "ID": 3.0, "naam": "tekst" }, "geometry": { "type": "Point", "coordinates": [ -58.3704125010713, -34.603013905923149 ] } }
+{ "type": "Feature", "properties": { "ID": 1.0, "naam": "Punt waarop de Indus rivier & Chenab rivier samen komen."}, "geometry": { "type": "Point", "coordinates": [ 70.68174363, 29.10872619 ] } },
+{ "type": "Feature", "properties": { "ID": 2.0, "naam": "Punt waarop de Indus rivier & Soan rivier samen komen." }, "geometry": { "type": "Point", "coordinates": [ 71.71992302, 33.02171429 ] } },
+{ "type": "Feature", "properties": { "ID": 3.0, "naam": "Punt waarop de Indus rivier & Kabul rivier samen komen." }, "geometry": { "type": "Point", "coordinates": [ 72.2350502, 33.91451734 ] } },
+{ "type": "Feature", "properties": { "ID": 4.0, "naam": "Punt waarop de Indus rivier & Kurram rivier samen komen." }, "geometry": { "type": "Point", "coordinates": [ 71.35546446, 32.612972 ] } }
 ]
 }
